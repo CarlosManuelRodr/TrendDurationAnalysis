560cace178f58789c82c423042d4454aedbd78a1
Carlos Manuel <fis.carlosmanuel@gmail.com>
2021-04-16 20:27:33 -0500

    Update scheduled_update.py

--- WebPage/scheduled_update.py
+++ WebPage/scheduled_update.py
@@ -2,8 +2,8 @@ import schedule
 import time
 from update_analysis import UpdateAnalysis
 
-schedule.every().day.at("16:00").do(UpdateAnalysis())
+schedule.every().day.at("16:00").do(UpdateAnalysis)
 
 while True:
     schedule.run_pending()
-    time.sleep(10)
+    time.sleep(60)
