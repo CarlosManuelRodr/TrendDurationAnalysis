ca22d6fa85b4ca045e0db0efc1c41e667355f626
Carlos Manuel <fis.carlosmanuel@gmail.com>
2021-04-16 19:59:19 -0500

    Configure server

--- WebPage/start_server.sh
+++ WebPage/start_server.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export FLASK_APP=mainpage.py
-flask run --host=0.0.0.0
+authbind --deep gunicorn --bind 0.0.0.0:80 mainpage:app
--- WebPage/wsgi.py
+++
@@ -1,4 +0,0 @@
-from mainpage import app
-
-if __name__ == "__main__":
-    app.run()
